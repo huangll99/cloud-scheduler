0a710552f997b27630e1095ef04fd774b51146e3
hll99 <huangll99@163.com>
2020-04-02 17:46:20 +0800

    commit

--- src/main/java/com/gsafety/scheduler/SchedulerApplication.java
+++ src/main/java/com/gsafety/scheduler/SchedulerApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SchedulerApplication {
 
   public static void main(String[] args) {
+
     SpringApplication.run(SchedulerApplication.class, args);
   }
 }
